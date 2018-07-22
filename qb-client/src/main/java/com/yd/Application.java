73f5a3df7fd7d9a80445051e7f4e84b5c9667211
myNameYD <838237306@qq.com>
2018-07-23 00:38:56 +0800

    配置eureka

--- qb-client/src/main/java/com/yd/Application.java
+++ qb-client/src/main/java/com/yd/Application.java
@@ -1,13 +1,13 @@
 package com.yd;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.yd.dao")
-@EnableCaching
+// @MapperScan(basePackages = "com.yd.dao")
+// @EnableCaching
+@EnableEurekaClient
 public class Application {
 
 
