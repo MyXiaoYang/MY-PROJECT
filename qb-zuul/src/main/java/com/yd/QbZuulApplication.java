7717ddfbad1a58f4d74f928fbb5e85166abfbe9e
myNameYD <838237306@qq.com>
2018-07-29 23:02:02 +0800

    config配置优化

--- qb-zuul/src/main/java/com/yd/QbZuulApplication.java
+++ qb-zuul/src/main/java/com/yd/QbZuulApplication.java
@@ -2,6 +2,9 @@ package com.yd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *  网关启动入口
@@ -10,9 +13,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author    YD
  */
 @SpringBootApplication
+@EnableZuulProxy
 public class QbZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QbZuulApplication.class, args);
 	}
+
 }
