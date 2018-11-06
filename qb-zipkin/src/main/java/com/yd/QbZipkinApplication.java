6cac911fa3292f33cf4df224f1435bb00be0b7df
da.yang <da.yang@shoptps.com>
2018-11-06 11:25:07 +0800

    添加zipkin工程

---
+++ qb-zipkin/src/main/java/com/yd/QbZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.yd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class QbZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QbZipkinApplication.class, args);
+    }
+}
